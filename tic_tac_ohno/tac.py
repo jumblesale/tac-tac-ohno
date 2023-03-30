59bc54c994c70923f28d994b2e9324ac40ce15a2
Rob Sollars <rob.sollars@thoughtworks.com>
2023-03-30 15:15:55 +0200

    adds a passing test for testing if the tac game is complete

--- tic_tac_ohno/tac.py
+++ tic_tac_ohno/tac.py
@@ -4,7 +4,9 @@ import pytest
 
 
 def is_the_game_complete(state: str) -> bool:
-    return True
+    unique_characters = set(''.join(state.split('\n')))
+    unique_characters.discard('*')
+    return len(unique_characters) < 2
 
 
 def a_state_with_rows(rows: List[str]) -> str:
@@ -13,17 +15,19 @@ def a_state_with_rows(rows: List[str]) -> str:
 
 complete_games = [a_state_with_rows(x) for x in [
     ['***', '*&*', '***'],
-    ['&&&', '&&&', '&&&'],
-]]
+    ['***', '***', '***'],
+    ['&&&', '&&&', '&&&'], ]]
 
 incomplete_games = [a_state_with_rows(x) for x in [
     ['***', '***', '*0q'],
-    ['***', '***', '***'],
-    ['ppp', 'ppp', 'ppy'],
-]]
+    ['ppp', 'ppp', 'ppy'], ]]
 
 
 @pytest.mark.parametrize("state", complete_games)
 def test_it_returns_true_when_only_one_icon_is_present(state):
     assert is_the_game_complete(state) is True
 
+
+@pytest.mark.parametrize("state", incomplete_games)
+def test_it_returns_true_when_only_one_icon_is_present(state):
+    assert is_the_game_complete(state) is False
