6cc0a064ce475ea5d29dcd2f6d84d624665e7e72
Rob Sollars <rob.sollars@thoughtworks.com>
2023-03-30 17:44:36 +0200

    added passing tests for filling in rows and columns

--- tic_tac_ohno/tac.py
+++ tic_tac_ohno/tac.py
@@ -1,3 +1,4 @@
+import functools
 from typing import List, Literal
 
 import pytest
@@ -11,19 +12,26 @@ def is_the_game_complete(state: str) -> bool:
     return len(unique_characters) < 2
 
 
-def move(
-        state:             str,
-        column_or_row:     ColumnOrRow,
+def move(state:             str,
+         column_or_row:     ColumnOrRow,
+         player_icon:       str,
+         other_player_icon: str,
+         index:             int, ) -> str:
+    matrix = state.split('\n')
+    partial_transform_row = functools.partial(transform_row, player_icon, other_player_icon, index)
+    if column_or_row is 'c':
+        updated_matrix = transpose(partial_transform_row(transpose(matrix)))
+    else:
+        updated_matrix = partial_transform_row(matrix)
+    return '\n'.join(updated_matrix)
+
+
+def transform_row(
         player_icon:       str,
         other_player_icon: str,
-        index:             int, ) -> str:
-    if column_or_row is not 'c':
-        raise Exception
-    transposed = transpose(state.split('\n'))
-    new_line = transform_line(transposed[index], player_icon, other_player_icon)
-    transposed[index] = new_line
-    return '\n'.join(transpose(transposed))
-    # return '\n'.join(['**%*', '%&*%', '&&&*', '*%%&', ])
+        index:             int,
+        matrix:            List[str], ) -> List[str]:
+    return replace(index, matrix, transform_line(player_icon, other_player_icon, matrix[index]))
 
 
 def transpose(matrix: List[str]) -> List[str]:
@@ -31,10 +39,7 @@ def transpose(matrix: List[str]) -> List[str]:
             for i in range(len(matrix[0]))]
 
 
-def transform_line(
-        line:              str,
-        player_icon:       str,
-        other_player_icon: str, ) -> str:
+def transform_line(player_icon: str, other_player_icon: str, line: str) -> str:
     new_line = ''
     transform_map = {
         player_icon:       '*',
@@ -83,7 +88,7 @@ line_transformations = [
 
 @pytest.mark.parametrize("initial_state, expected_state, icon_1, icon_2", line_transformations)
 def test_it_converts_lines(initial_state: str, expected_state: str, icon_1: str, icon_2: str):
-    assert transform_line(initial_state, icon_1, icon_2) == expected_state
+    assert transform_line(icon_1, icon_2, initial_state) == expected_state
 
 
 def test_it_fills_in_columns():
@@ -111,7 +116,7 @@ def test_it_fills_in_rows():
     initial_state = a_state_with_rows([
         '***^', '^^@*', '^*@@', '**^@', ])
     expected_state = '\n'.join([
-        '***^', '^^@*', '*@^@', '*%%&', ])
+        '***^', '^^@*', '*@^@', '**^@', ])
 
     # act
     result = move(initial_state, 'r', '^', '@', 2)
@@ -127,6 +132,23 @@ def test_it_transposes():
 
     # act
     result = transpose(matrix)
+
+    # assert
+    assert result == expected
+
+
+def replace(index: int, matrix: List[str], replacement: str) -> List[str]:
+    matrix[index] = replacement
+    return matrix
+
+
+def test_it_replaces_lines():
+    # arrange
+    matrix = ['12345', '67890', '22345', '27890', ]
+    expected = ['12345', '67890', '*****', '27890', ]
+
+    # act
+    result = replace(2, matrix, '*****')
 
     # assert
     assert result == expected
