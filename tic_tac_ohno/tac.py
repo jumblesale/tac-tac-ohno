c2ce2df24f944d4b9c4a768413360b041b760899
Rob Sollars <rob.sollars@thoughtworks.com>
2023-03-30 15:06:05 +0200

    adds code for complete states

--- tic_tac_ohno/tac.py
+++ tic_tac_ohno/tac.py
@@ -1,10 +1,31 @@
+from typing import List
+
+import pytest
+
+
 def is_the_game_complete(state: str) -> bool:
-    return False
+    return True
 
 
-def test_it_returns_true_when_only_one_icon_is_present():
+def a_state_with_rows(rows: List[str]) -> str:
+    return '\n'.join(rows)
+
+
+complete_games = [a_state_with_rows(x) for x in [
+    ['***', '*&*', '***'],
+    ['&&&', '&&&', '&&&'],
+]]
+
+incomplete_games = [a_state_with_rows(x) for x in [
+    ['***', '***', '*0q'],
+    ['***', '***', '***'],
+    ['ppp', 'ppp', 'ppy'],
+]]
+
+
+@pytest.mark.parametrize("state", complete_games)
+def test_it_returns_true_when_only_one_icon_is_present(state):
     # arrange
-    state = ['***', '*&*', '***']
     expected_result = True
 
     # act
